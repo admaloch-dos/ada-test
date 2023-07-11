eb803c778b6340a90a175de3a3d7d1bfe0121f57
davis maloch (fl. memory) <133890108+admaloch-dos@users.noreply.github.com>
2023-07-11 09:21:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1066,7 +1066,7 @@ $(document).bind('mousemove', function (e) {
 
   // console.log(e.pageY)
   $('#top_mask').css({
-    top: e.pageY - 1400
+    top: e.pageY - 1420
   });
 
   $('#bottom_mask').css({
