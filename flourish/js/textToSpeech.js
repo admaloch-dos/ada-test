ef3ae3d2f17e90fb8799b7c39b7a8925283e013c
Davis Maloch (Fl. Memory) <133890108+admaloch-dos@users.noreply.github.com>
2024-02-09 16:59:51 -0500

    Add files via upload

--- flourish/js/textToSpeech.js
+++ flourish/js/textToSpeech.js
@@ -4,14 +4,15 @@ $(function () {
   $('[id="ToggleTTS_click"]').change(function () {
     if ($(this).is(':checked')) {
       toggleSpeechOn()
-      createSpeechItems()
-      speechItemHandler()
     } else {
       toggleSpeechOff()
     }
-    checkIfWidgetActive()
   });
 });
+
+let voiceSelectCurrVal = ''
+
+// voiceSelect.addEventListener("change", (event) => voiceSelect = event.target.value);
 
 const toggleSpeechOn = () => {
   $(".audio_state").hide()
@@ -21,6 +22,9 @@ const toggleSpeechOn = () => {
   $('#reset-voice-settings').fadeIn()
   addWidgetControls('ToggleTTS_click', 'Text to speech')
   widgetItemObj.isSpeech = true
+  addAudioPlayers('p, :header, button, td, th, label, strong, a, li, span, select')
+  speechItemHandler()
+  checkIfWidgetActive()
 }
 
 const toggleSpeechOff = () => {
@@ -34,6 +38,7 @@ const toggleSpeechOff = () => {
   }, 500);
   removeWidgetControls(['ToggleTTS_click'])
   widgetItemObj.isSpeech = false
+  checkIfWidgetActive()
 }
 
 
@@ -46,212 +51,200 @@ const resetSpeech = () => {
       $(this).addClass('inactive-item')
       $(this).removeClass('audio-paused audio-playing curr-active-item')
       $(this).addClass('audio-inactive ')
-      $(this).children('.trigger-audio-icon').eq(0).attr("src", "./flourish/img/trigger-audio.svg");
+      $(this).children('.trigger-audio-icon').first().attr("src", "./flourish/img/trigger-audio.svg");
     } else {
       $(this).addClass('d-none')
     }
   });
 }
 
-const createSpeechItems = () => {
-  addAudioPlayers()
-  addAudioPlayerContainer()
-  removeParagraphLinks()
-}
 
-const addAudioPlayers = () => {
-  $('p, h1, h2, h3, h4, h5, h6, button, td, th, label, strong, a, li, span').not("#language-btn-modal-header *").each(function () {
+
+
+const addAudioPlayers = (selectors) => {
+  $(selectors).not("#language-btn-modal-header *").each(function () {
     const itemDisplay = window.getComputedStyle(this, null).display
     const itemOpacity = window.getComputedStyle(this, null).opacity
     const currTextItem = $(this).text()
     const itemType = this.nodeName
 
-    if (currTextItem && currTextItem.replaceAll(/\s/g, '') !== '' && itemDisplay !== 'none' && itemOpacity !== '0') {
-      if (itemType === 'SPAN' || itemType === 'LI') {
+    if (currTextItem && currTextItem.replaceAll(/\s/g, '') !== '' && currTextItem.replaceAll(/\s/g, '') !== 'inherit' && currTextItem.replaceAll(/\s/g, '').length > 1 && itemDisplay !== 'none' && itemOpacity !== '0' && this.id !== 'preset-color-btn' && this.id !== 'custom-color-btn' && !$(this).parent().hasClass('TTS_content')) {
+
+      if (itemType === 'SPAN' || itemType === 'LI' || itemType === 'TD' || itemType === 'TH') {
         if (!this.firstElementChild) {
-          $('<div class="audio_state">\
-            <button class="trigger-audio play-pause inactive-item audio-inactive" title="Trigger audio"><img alt="Text-to-speech icon"  class="trigger-audio-icon" src="./flourish/img/trigger-audio.svg" alt="trigger speech"></button>\<button class="trigger-audio reset-audio-btn d-none" title="Cancel"><img  src="./flourish/img/reset.png" alt="Reset text-to-speech icon"></button>\</div>').insertAfter(this);
+          generateAudioPlayer(this)
+        }
+      }else if (itemType === 'SELECT') {
+        if(this.value !== 'inherit') {
+          generateAudioPlayer(this)
+        }
+      } else if (itemType === 'A') {
+        if (!this.closest('p')) {
+          generateAudioPlayer(this)
         }
       } else {
-        $('<div class="audio_state">\
-      <button class="trigger-audio play-pause inactive-item audio-inactive" title="Trigger audio"><img alt="Text-to-speech icon"  class="trigger-audio-icon" src="./flourish/img/trigger-audio.svg" alt="trigger speech"></button>\<button class="trigger-audio reset-audio-btn d-none" title="Cancel"><img  src="./flourish/img/reset.png" alt="Reset text-to-speech icon"></button>\</div>').insertAfter(this);
+        generateAudioPlayer(this)
       }
+
     }
+    if (this.classList.contains('hidden-span')) {
+      generateAudioPlayer(this)
+    }
+
+
   })
 }
+// if(!$(item).parent().hasClass('TTS_content'))
+const generateAudioPlayer = (item) => {
 
-const addAudioPlayerContainer = () => {
-  $('div.audio_state').each(function () {
-    $(this)
-      .prev()
-      .addBack()
-      .wrapAll('<section class="TTS_content"></section>');
-  });
+  $(item).wrap('<section class="TTS_content"></section>');
+  $('<div class="audio_state">\
+  <button class="trigger-audio play-pause inactive-item audio-inactive" title="Trigger audio"><img data-id="hide-img-hover" alt="Text-to-speech icon"  class="trigger-audio-icon" src="./flourish/img/trigger-audio.svg" alt="trigger speech"></button>\<button class="trigger-audio reset-audio-btn d-none" title="Cancel"><img data-id="hide-img-hover"  src="./flourish/img/reset.png" alt="Reset text-to-speech icon"></button>\</div>').insertAfter(item);
+
 }
 
-const removeParagraphLinks = () => {
-  document.querySelectorAll('p').forEach(item => {
-    item.querySelectorAll('a').forEach((link, i) => {
-      if (link) {
-        let ttsParent = link.closest('.TTS_content')
-        if (ttsParent) {
-          let audioItem = ttsParent.querySelector('.audio_state')
-          if (audioItem) {
-            audioItem.style.display = 'none'
-          }
-        }
-      }
-    })
-  })
-}
+
+
 
 /***** handler for speech items -- the speech btn and the play/pause/reset all have trigger audio class.. tehse conditionals test whcih one is which *****/
+let speechInterval
 const speechItemHandler = () => {
-  let speechInterval
   $('div.audio_state .trigger-audio').each(function (index) {
-    $(this).click(function (e) {
+    $(this).unbind('click').click(function (e) {
+      // console.log(this)
+      // e.stopPropagation()
+      e.preventDefault()
       clearInterval(speechInterval)
       if ($(this).hasClass('play-pause')) {
         if (!$(this).hasClass('curr-active-item') && !$(this).hasClass('reset-audio-btn')) {
-          console.log('item activated and auto played. press pause to pause audio')
-          resetSpeech()
-          $(this).children('.trigger-audio-icon').eq(0).attr("src", "./flourish/img/pause.png");
-          $(this).addClass('curr-active-item')
-          $(this).removeClass('inactive-item')
-          $(this).closest('.audio_state').children('.reset-audio-btn').eq(0).removeClass('d-none')
-        }
-        if ($(this).hasClass('audio-playing')) {
-          console.log('pressed pause button -- press play to continue')
-          // console.log(synth)
-          $(this).removeClass('audio-inactive audio-playing')
-          $(this).addClass('audio-paused')
-          $(this).children('.trigger-audio-icon').eq(0).attr("src", "./flourish/img/play.png");
-          synth.pause()
-          speechInterval = setInterval(() => {
-
-              console.log('15 sec pause hack just ran')
-              speechSynthesis.resume();
-              speechSynthesis.pause()
-
-
-          }, 14000);
-
-        } else if ($(this).hasClass('audio-inactive')) {
-          console.log('pressed play btn.. audio was inactive and will now play from the beginning. press pause to pause')
-          $(this).children('.trigger-audio-icon').eq(0).attr("src", "./flourish/img/pause.png");
-          $(this).removeClass('audio-inactive audio-paused')
-          $(this).addClass('audio-playing')
-          synth.cancel();
-          ssu.text = $(this).parent("div.audio_state").prev("p, h1, h2, h3, h4, h5, h6, button, span, td, th, label, a, strong, li").text();
-          ssu.volume = parseFloat(volumeInput.value / 10);
-          ssu.rate = parseFloat(rateInput.value / 5);
-          ssu.pitch = parseFloat(pitchInput.value / 5 + .01);
-          if (voiceSelect.value) {
-            ssu.voice = speechSynthesis.getVoices().filter(function (voice) {
-              synth.cancel();
-              $(this).removeClass('audio-paused audio-playing')
-              $(this).addClass('audio-inactive ')
-              return voice.name == voiceSelect.value;
-            })[0];
-          }
-          $(this).addClass('audio-playing')
-          synth.speak(ssu);
-
-          speechInterval = setInterval(() => {
-            if (!speechSynthesis.speaking) {
-              clearInterval(speechInterval);
-            } else {
-              console.log('15 sec play hack just ran')
-              speechSynthesis.pause();
-              speechSynthesis.resume();
-            }
-          }, 14000);
-          ssu.addEventListener("end", (event) => {
-            console.log('audio ended and is now reset. Press play to start from the beginning')
-            synth.cancel();
-            if ($(this).hasClass('curr-active-item')) {
-              $(this).removeClass('audio-paused audio-playing')
-              $(this).addClass('audio-inactive ')
-              $(this).children('.trigger-audio-icon').eq(0).attr("src", "./flourish/img/play.png");
-            }
-          });
-
+          initAudioHandler(this)
+        } if ($(this).hasClass('audio-inactive')) {
+          startAudioHandler(this)
+        } else if ($(this).hasClass('audio-playing')) {
+          pauseAudioHandler(this)
         } else if ($(this).hasClass('audio-paused')) {
-          console.log('play btn pressed to resume active audio. press pause to pause')
-          console.log(synth)
-
-          $(this).children('.trigger-audio-icon').eq(0).attr("src", "./flourish/img/pause.png");
-          $(this).removeClass('audio-inactive audio-paused')
-          $(this).addClass('audio-playing')
-          synth.resume()
-
+          resumeAudioHandler(this)
         }
       } else {
-        console.log('reset btn clicked. Click play to restart from the beginning')
-        synth.cancel()
-        $(this).siblings('.play-pause').removeClass('audio-paused audio-playing')
-        $(this).siblings('.play-pause').addClass('audio-inactive')
-        $(this).siblings('.play-pause').children('.trigger-audio-icon').eq(0).attr("src", "./flourish/img/play.png");
+        resetAudioHandler(this)
       }
-      e.stopPropagation()
-      // fixBug2(this)
-      // fixSpeechBug(this)
-
-
-
+      e.stopPropagation(this)
     });
   });
 }
 
-const fixSpeechBug = (input) => {
-  let r = setInterval(() => {
-    if (!synth.speaking || $(input).hasClass('audio-paused')) {
-      clearInterval(r);
+const initAudioHandler = (item) => {
+  console.log('init audio ran')
+  resetSpeech()
+  $(item).children('.trigger-audio-icon').first().attr("src", "./flourish/img/pause.png");
+  $(item).addClass('curr-active-item').removeClass('inactive-item')
+  $(item).closest('.audio_state').children('.reset-audio-btn').first().removeClass('d-none')
+}
 
-    } else if (synth.speaking || !$(input).hasClass('audio-paused')) {
-      synth.pause();
-      synth.resume();
-    }
-  }, 14000);
+const startAudioHandler = (item) => {
+  console.log('start audio ran')
+  synth.cancel();
+  speechSynthesisParams(item)
+  synth.speak(ssu);
+  startAudioBtns(item)
+  forceSpeechInterval()
+  resetCompletedAudio(item)
 }
 
 
-const fixBug2 = (input) => {
-  let timer
-  ssu.onstart = () => {
-    // detection is up to you for this article as
-    // this is an own huge topic for itself
-    const currOS = getOS()
-    if (currOS !== 'Android') {
-      resumeInfinity(ssu)
-    }
+const startAudioBtns = (item) => {
+  $(item).children('.trigger-audio-icon').first().attr("src", "./flourish/img/pause.png");
+  $(item).removeClass('audio-inactive audio-paused').addClass('audio-playing')
+}
+
+
+
+const speechSynthesisParams = (item) => {
+  const clear = () => { clearInterval(speechInterval) }
+  const itemType = item.closest('.TTS_content').firstChild.nodeName.toLowerCase()
+console.log(itemType)
+  if (itemType !== 'select') {
+    ssu.text = $(item).parent("div.audio_state").prev(itemType).text();
+  } else {
+    ssu.text = $(item).parent().prev()[0].value
   }
 
-  const clear = () => { clearTimeout(timer) }
-
+  ssu.volume = parseFloat(volumeInput.value / 10);
+  ssu.rate = parseFloat(rateInput.value / 5);
+  ssu.pitch = parseFloat(pitchInput.value / 5 + .01);
   ssu.onerror = clear
-  ssu.onend = clear
+  if (voiceSelect.value) {
+    ssu.voice = speechSynthesis.getVoices().filter(function (voice) {
+      synth.cancel();
+      $(item).removeClass('audio-paused audio-playing')
+      $(item).addClass('audio-inactive ')
+      return voice.name == voiceSelect.value;
+    })[0];
+  }
 
-  const resumeInfinity = (target) => {
-    if ($(input).hasClass('audio-paused')) {
-      console.log('it is paused')
-      return clear()
-    }
-    else {
-      // if (!target && timer) { return clear() }
+}
 
-      synth.pause()
-      synth.resume()
-
-      timer = setTimeout(function () {
-        resumeInfinity(target)
-      }, 5000)
-    }
-
-    // prevent memory-leak in case ssu is deleted, while this is ongoing
-
+const forceSpeechInterval = (item) => {
+  // on chrome speechSynthesis has a bug where it stops playing if the passage is longer than 15 seconds
+  const currOS = getOS()
+  if (currOS !== 'Android') {
+    speechInterval = setInterval(() => {
+      if (!speechSynthesis.speaking) {
+        clearInterval(speechInterval);
+      } else {
+        console.log('15 sec interval force continue')
+        speechSynthesis.pause();
+        speechSynthesis.resume();
+      }
+    }, 14000);
   }
 }
+
+const resetCompletedAudio = (item) => {
+  ssu.addEventListener("end", (event) => {
+    console.log('audio ended and is now reset. Press play to start from the beginning')
+    synth.cancel();
+    if ($(item).hasClass('curr-active-item')) {
+      $(item).removeClass('audio-paused audio-playing')
+      $(item).addClass('audio-inactive ')
+      $(item).children('.trigger-audio-icon').first().attr("src", "./flourish/img/play.png");
+    }
+  });
+}
+
+const resumeAudioHandler = (item) => {
+  console.log('resume audio ran')
+  startAudioBtns(item)
+  synth.resume()
+  forceSpeechInterval()
+}
+
+const pauseAudioHandler = (item) => {
+  console.log('pause audio ran')
+  $(item).removeClass('audio-inactive audio-playing')
+  $(item).addClass('audio-paused')
+  $(item).children('.trigger-audio-icon').first().attr("src", "./flourish/img/play.png");
+  synth.pause()
+  const currOS = getOS()
+  if (currOS !== 'Android') {
+    speechInterval = setInterval(() => {
+      console.log('15 sec pause hack just ran')
+      speechSynthesis.resume();
+      speechSynthesis.pause()
+    }, 14000);
+  }
+}
+
+const resetAudioHandler = (item) => {
+  console.log('reset audio ran')
+  synth.cancel()
+  $(item).siblings('.play-pause').removeClass('audio-paused audio-playing').addClass('audio-inactive')
+  $(item).siblings('.play-pause').children('.trigger-audio-icon').first().attr("src", "./flourish/img/play.png");
+}
+
+
+
+
 
 const removeAudioState = () => {
   document.querySelectorAll('.audio_state').forEach(item => {
@@ -289,15 +282,26 @@ document.querySelector('#reset-flourish').addEventListener('click', () => {
   loadVoices()
 })
 
+
+
+document.querySelector('#flourish-more-languages-btn').addEventListener('click', () => {
+  languageModalSpeechHandler('#all-languages-modal-content .translate-language-span')
+})
+
 document.querySelectorAll('.lang-filter').forEach(item => {
   item.addEventListener('click', () => {
-    resetTextToSpeech()
+    languageModalSpeechHandler('#all-languages-modal-content .translate-language-span, #all-languages-modal-content h5, #all-languages-modal-content h6')
   })
 })
 
-document.querySelector('#flourish-more-languages-btn').addEventListener('click', () => {
-  resetTextToSpeech()
-})
+const languageModalSpeechHandler = (selectors) => {
+  setTimeout(() => {
+    if (document.body.classList.contains('TTS_click_enabled')) {
+      addAudioPlayers(selectors)
+      speechItemHandler()
+    }
+  }, 200)
+}
 
 const speechCookieHandler = () => {
   if ($.cookie('TTS_click_enabled') === 'true') {
