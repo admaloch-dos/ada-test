5eb7bd6f6e73f21901d27f8bede1c1ee34eb9d3e
Davis Maloch (Fl. Memory) <133890108+admaloch-dos@users.noreply.github.com>
2024-11-10 11:34:50 -0500

    Add files via upload

--- flourish/flourish.js
+++ flourish/flourish.js
@@ -1,4 +1,97 @@
+// code related to checking cookies/data/if widget is active or not
+// data to determine if widget is still active / widget active items
+let isWidgetActive = false
+const cookiesArr = ['TTS_click_enabled', 'DarkContrastBackground', 'DesaturateBackground', 'InvertBackground', 'HighSaturationBackground',
+  'LowSaturationBackground', 'AEC', '1P_JAR', 'NID', 'DV', 'translateLanguage', 'text-magnify-color-swatch', 'text-magnify-size-input', 'img-magnify-size-input', 'img-magnify-color-swatch', 'text-magnify-color-swatch', '.img-magnify-color-swatch', 'edit-reading-guide', 'edit-reading-mask', 'TTS_click_enabled', 'googtrans', 'readingMaskVal', 'BackgroundColorCookie', 'TextColorCookie', 'LinkColorCookie', 'TextMagnifier', 'HighlightLinks', 'ImageDescription', 'HighlightHover', 'FontSizeCookie', 'FT_Baskerville', 'FT_Dyslexic', 'CursorEnlargeCookie', 'PhotoSens', 'ReadingMask', 'CursorGuide', 'TTS_click_enabled', 'LinpageHeightVal', 'WordSpaceVal', 'LetterSpaceVal', 'speechPitch', 'speechRate', 'speechVol', 'voiceCookie']
+let widgetItemObj = {
+  isHighlighted: false,
+  isOutlined: false,
+  isTextMag: false,
+  isImgMag: false,
+  isFontBig: false,
+  isFontChanged: false,
+  isCursorBig: false,
+  isLineHeightChanged: false,
+  isWordSpaceChanged: false,
+  isLetterSpaceChanged: false,
+  'isDarkContrast': false,
+  'isDesaturated': false,
+  'isInverted': false,
+  'isHighSat': false,
+  'isLowSat': false,
+  isTextColorChanged: false,
+  isBackColorChanged: false,
+  isLinkColorChanged: false,
+  isPhotoSens: false,
+  isReadingMask: false,
+  isReadingGuide: false,
+  isSpeech: false,
+  isDyslexicFont: false,
+  isBaskervilleFont: false,
+  isTranslated: false,
+}
 
+// function to reset all cookies
+const removeAllCookies = () => {
+  for (let i = 0; i < cookiesArr.length; i++) {
+    $.removeCookie(cookiesArr[i]);
+  }
+}
+
+// reset widgetItemObj on cookie load
+const isCookieActive = (input, value) => {
+  if (input && input !== value) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const areCookiesSet = () => {
+  let updateCookies = widgetItemObj
+  updateCookies.isHighlighted = isCookieActive($.cookie("HighlightHover"), 'false')
+  updateCookies.isOutlined = isCookieActive($.cookie("HighlightLinks"), 'false')
+  updateCookies.isTextMag = isCookieActive($.cookie("TextMagnifier"), 'false')
+  updateCookies.isImgMag = isCookieActive($.cookie("ImageDescription"), 'false')
+  updateCookies.isFontBig = isCookieActive($.cookie("FontSizeCookie"), 'null')
+  updateCookies.isCursorBig = isCookieActive($.cookie("CursorEnlargeCookie"), 'null')
+  updateCookies.isLineHeightChanged = isCookieActive($.cookie("LinpageHeightVal"), 'inherit')
+  updateCookies.isWordSpaceChanged = isCookieActive($.cookie("WordSpaceVal"), 'inherit')
+  updateCookies.isLetterSpaceChanged = isCookieActive($.cookie("LetterSpaceVal"), 'inherit')
+  updateCookies.isLowSat = isCookieActive($.cookie("LowSaturationBackgroundCookie"), 'null')
+  updateCookies.isHighSat = isCookieActive($.cookie("HighSaturationBackgroundCookie"), 'null')
+  updateCookies.isInverted = isCookieActive($.cookie("InvertBackground"), 'null')
+  updateCookies.isDesaturated = isCookieActive($.cookie("DesaturatedBackground"), 'null')
+  updateCookies.isDarkContrast = isCookieActive($.cookie("DarkContrastBackground"), 'null')
+  updateCookies.isTextColorChanged = isCookieActive($.cookie("TextColorCookie"), 'false')
+  updateCookies.isBackColorChanged = isCookieActive($.cookie("BackgroundColorCookie"), 'false')
+  updateCookies.isLinkColorChanged = isCookieActive($.cookie("LinkColorCookie"), 'false')
+  updateCookies.isPhotoSens = isCookieActive($.cookie("PhotoSens"), 'false')
+  updateCookies.isReadingMask = isCookieActive($.cookie("ReadingMask"), 'false')
+  updateCookies.isReadingGuide = isCookieActive($.cookie("CursorGuide"), 'false')
+  updateCookies.isSpeech = isCookieActive($.cookie("TTS_click_enabled"), 'false')
+  updateCookies.isBaskervilleFont = isCookieActive($.cookie("BaskervilleFontCookie"), 'null')
+  updateCookies.isDyslexicFont = isCookieActive($.cookie("DyslexicFontCookie"), 'null')
+  return updateCookies
+}
+widgetItemObj = areCookiesSet()
+
+// check if widget items eval to true if so  fade in helper box
+const checkIfWidgetActive = () => {
+  if (Object.values(widgetItemObj).indexOf(true) > -1) {
+    isWidgetActive = true
+    $('#flourish_check_icon').fadeIn()
+    $('#toggle-flourish-list, #reset-flourish').fadeIn()
+  } else {
+    isWidgetActive = false
+    $('#flourish_check_icon').fadeOut()
+    $('#toggle-flourish-list, #reset-flourish, #item-delete-container').fadeOut()
+    $("#toggle-flourish-list").removeClass("fa-toggle-on");
+    $("#toggle-flourish-list").addClass("fa-toggle-off");
+    $.removeCookie('deleteContainerActive');
+  }
+}
+checkIfWidgetActive()
 
 
 //insert html for flourish to dom
@@ -60,7 +153,7 @@ const createFlourish = () => {
     <main id="flourish-modal-content" class="modal_content">
         <div class="flourish-modal-container d-flex justify-content-center flex-column align-items-center" role="container for flourish modal content">
             <header class="modal_header">
-                <img id="flourish-logo-icon" class="img-fluid flourish-icon-style mr-2 " loading="lazy"
+                <img id="flourish-logo-icon" width="50" height="50" class="img-fluid flourish-icon-style mr-2 " loading="lazy"
                     src="./flourish/img/flourish-logo-square-sm-white-text.png" aria-label="flourish logo icon" alt="Flourish logo icon" />
                 <div class="row flourish-header-row">
                     <div id="flourish-header-title"
@@ -1598,7 +1691,7 @@ const createFlourish = () => {
                             class="flourish-sub-section flourish-contact-info d-flex flex-column justify-content-center align-items-center">
                             <div id="dos-statement"
                                 class="d-flex flex-column justify-content-center align-items-center">
-                                <img id="flourish-statement-icon" class="img-fluid flourish-icon-style text-center "
+                                <img id="flourish-statement-icon" width="35" height="35" class="img-fluid flourish-icon-style text-center "
                                     loading="lazy" src="./flourish/img/flourish-logo-square-notext.png"
                                     alt="Flourish logo icon" />
                                 <a class="text-center" id="flourish-statement-link"
@@ -1608,7 +1701,7 @@ const createFlourish = () => {
                             </div>
                             <div id="email-info"
                                 class=" d-flex flex-column justify-content-center align-items-center mb-2">
-                                <img id="flourish-email-icon" class="img-fluid flourish-icon-style " loading="lazy"
+                                <img id="flourish-email-icon" width="35" height="35" class="img-fluid flourish-icon-style " loading="lazy"
                                     src="./flourish/img/email.png" alt="Email icon" />
                                 <p class=" flourish-statement-info-p text-center"> For questions and
                                     concerns, email us at <a
@@ -1631,12 +1724,12 @@ const createFlourish = () => {
                         </div>
                         <div id="flourish-logos"
                             class="col-12 d-flex flex-column flex-md-row justify-content-center align-items-center">
-                            <a href="https://dos.fl.gov/"><img id="flourish-dos-logo" class="" loading="lazy"
+                            <a href="https://dos.fl.gov/"><img id="flourish-dos-logo" width="250" height="100" class="" loading="lazy"
                                     src="./flourish/img/dos-logo.png" alt="Florida Department of State logo "></a>
-                            <a href="https://dos.fl.gov/library-archives/"> <img id="flourish-dlis-logo" class=""
+                            <a href="https://dos.fl.gov/library-archives/"> <img id="flourish-dlis-logo"  width="250" height="100" class=""
                                     loading="lazy" src="./flourish/img/dlis-logo.png"
                                     alt="Division of Library and Information Services logo"></a>
-                            <a href="https://www.imls.gov/"><img id="flourish-imls-logo" class="" loading="lazy"
+                            <a href="https://www.imls.gov/"><img id="flourish-imls-logo" class="" width="250" height="100" loading="lazy"
                                     src="./flourish/img/imls-logo.png"
                                     alt="Institute of Museum and Library Services logo"></a>
                         </div>
@@ -5005,100 +5098,7 @@ const resetAudioHandler = (item) => {
 
 }
 
-// code related to checking cookies/data/if widget is active or not
-// data to determine if widget is still active / widget active items
-let isWidgetActive = false
-const cookiesArr = ['TTS_click_enabled', 'DarkContrastBackground', 'DesaturateBackground', 'InvertBackground', 'HighSaturationBackground',
-  'LowSaturationBackground', 'AEC', '1P_JAR', 'NID', 'DV', 'translateLanguage', 'text-magnify-color-swatch', 'text-magnify-size-input', 'img-magnify-size-input', 'img-magnify-color-swatch', 'text-magnify-color-swatch', '.img-magnify-color-swatch', 'edit-reading-guide', 'edit-reading-mask', 'TTS_click_enabled', 'googtrans', 'readingMaskVal', 'BackgroundColorCookie', 'TextColorCookie', 'LinkColorCookie', 'TextMagnifier', 'HighlightLinks', 'ImageDescription', 'HighlightHover', 'FontSizeCookie', 'FT_Baskerville', 'FT_Dyslexic', 'CursorEnlargeCookie', 'PhotoSens', 'ReadingMask', 'CursorGuide', 'TTS_click_enabled', 'LinpageHeightVal', 'WordSpaceVal', 'LetterSpaceVal', 'speechPitch', 'speechRate', 'speechVol', 'voiceCookie']
-let widgetItemObj = {
-  isHighlighted: false,
-  isOutlined: false,
-  isTextMag: false,
-  isImgMag: false,
-  isFontBig: false,
-  isFontChanged: false,
-  isCursorBig: false,
-  isLineHeightChanged: false,
-  isWordSpaceChanged: false,
-  isLetterSpaceChanged: false,
-  'isDarkContrast': false,
-  'isDesaturated': false,
-  'isInverted': false,
-  'isHighSat': false,
-  'isLowSat': false,
-  isTextColorChanged: false,
-  isBackColorChanged: false,
-  isLinkColorChanged: false,
-  isPhotoSens: false,
-  isReadingMask: false,
-  isReadingGuide: false,
-  isSpeech: false,
-  isDyslexicFont: false,
-  isBaskervilleFont: false,
-  isTranslated: false,
-}
 
-// function to reset all cookies
-const removeAllCookies = () => {
-  for (let i = 0; i < cookiesArr.length; i++) {
-    $.removeCookie(cookiesArr[i]);
-  }
-}
-
-// reset widgetItemObj on cookie load
-const isCookieActive = (input, value) => {
-  if (input && input !== value) {
-    return true
-  } else {
-    return false
-  }
-}
-
-const areCookiesSet = () => {
-  let updateCookies = widgetItemObj
-  updateCookies.isHighlighted = isCookieActive($.cookie("HighlightHover"), 'false')
-  updateCookies.isOutlined = isCookieActive($.cookie("HighlightLinks"), 'false')
-  updateCookies.isTextMag = isCookieActive($.cookie("TextMagnifier"), 'false')
-  updateCookies.isImgMag = isCookieActive($.cookie("ImageDescription"), 'false')
-  updateCookies.isFontBig = isCookieActive($.cookie("FontSizeCookie"), 'null')
-  updateCookies.isCursorBig = isCookieActive($.cookie("CursorEnlargeCookie"), 'null')
-  updateCookies.isLineHeightChanged = isCookieActive($.cookie("LinpageHeightVal"), 'inherit')
-  updateCookies.isWordSpaceChanged = isCookieActive($.cookie("WordSpaceVal"), 'inherit')
-  updateCookies.isLetterSpaceChanged = isCookieActive($.cookie("LetterSpaceVal"), 'inherit')
-  updateCookies.isLowSat = isCookieActive($.cookie("LowSaturationBackgroundCookie"), 'null')
-  updateCookies.isHighSat = isCookieActive($.cookie("HighSaturationBackgroundCookie"), 'null')
-  updateCookies.isInverted = isCookieActive($.cookie("InvertBackground"), 'null')
-  updateCookies.isDesaturated = isCookieActive($.cookie("DesaturatedBackground"), 'null')
-  updateCookies.isDarkContrast = isCookieActive($.cookie("DarkContrastBackground"), 'null')
-  updateCookies.isTextColorChanged = isCookieActive($.cookie("TextColorCookie"), 'false')
-  updateCookies.isBackColorChanged = isCookieActive($.cookie("BackgroundColorCookie"), 'false')
-  updateCookies.isLinkColorChanged = isCookieActive($.cookie("LinkColorCookie"), 'false')
-  updateCookies.isPhotoSens = isCookieActive($.cookie("PhotoSens"), 'false')
-  updateCookies.isReadingMask = isCookieActive($.cookie("ReadingMask"), 'false')
-  updateCookies.isReadingGuide = isCookieActive($.cookie("CursorGuide"), 'false')
-  updateCookies.isSpeech = isCookieActive($.cookie("TTS_click_enabled"), 'false')
-  updateCookies.isBaskervilleFont = isCookieActive($.cookie("BaskervilleFontCookie"), 'null')
-  updateCookies.isDyslexicFont = isCookieActive($.cookie("DyslexicFontCookie"), 'null')
-  return updateCookies
-}
-widgetItemObj = areCookiesSet()
-
-// check if widget items eval to true if so  fade in helper box
-const checkIfWidgetActive = () => {
-  if (Object.values(widgetItemObj).indexOf(true) > -1) {
-    isWidgetActive = true
-    $('#flourish_check_icon').fadeIn()
-    $('#toggle-flourish-list, #reset-flourish').fadeIn()
-  } else {
-    isWidgetActive = false
-    $('#flourish_check_icon').fadeOut()
-    $('#toggle-flourish-list, #reset-flourish, #item-delete-container').fadeOut()
-    $("#toggle-flourish-list").removeClass("fa-toggle-on");
-    $("#toggle-flourish-list").addClass("fa-toggle-off");
-    $.removeCookie('deleteContainerActive');
-  }
-}
-checkIfWidgetActive()
 
 // active item add/delete section -------------->
 const addWidgetControls = (item, text) => {
