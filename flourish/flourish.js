763319a551cb147cee87824a818e254741e2f4a9
Davis Maloch (Fl. Memory) <133890108+admaloch-dos@users.noreply.github.com>
2025-02-01 11:53:42 -0600

    Add files via upload

--- flourish/flourish.js
+++ flourish/flourish.js
@@ -1,97 +1,4 @@
-// code related to checking cookies/data/if widget is active or not
-// data to determine if widget is still active / widget active items
-let isWidgetActive = false
-const cookiesArr = ['TTS_click_enabled', 'DarkContrastBackground', 'DesaturateBackground', 'InvertBackground', 'HighSaturationBackground',
-  'LowSaturationBackground', 'AEC', '1P_JAR', 'NID', 'DV', 'translateLanguage', 'text-magnify-color-swatch', 'text-magnify-size-input', 'img-magnify-size-input', 'img-magnify-color-swatch', 'text-magnify-color-swatch', '.img-magnify-color-swatch', 'edit-reading-guide', 'edit-reading-mask', 'TTS_click_enabled', 'googtrans', 'readingMaskVal', 'BackgroundColorCookie', 'TextColorCookie', 'LinkColorCookie', 'TextMagnifier', 'HighlightLinks', 'ImageDescription', 'HighlightHover', 'FontSizeCookie', 'FT_Baskerville', 'FT_Dyslexic', 'CursorEnlargeCookie', 'PhotoSens', 'ReadingMask', 'CursorGuide', 'TTS_click_enabled', 'LinpageHeightVal', 'WordSpaceVal', 'LetterSpaceVal', 'speechPitch', 'speechRate', 'speechVol', 'voiceCookie']
-let widgetItemObj = {
-  isHighlighted: false,
-  isOutlined: false,
-  isTextMag: false,
-  isImgMag: false,
-  isFontBig: false,
-  isFontChanged: false,
-  isCursorBig: false,
-  isLineHeightChanged: false,
-  isWordSpaceChanged: false,
-  isLetterSpaceChanged: false,
-  'isDarkContrast': false,
-  'isDesaturated': false,
-  'isInverted': false,
-  'isHighSat': false,
-  'isLowSat': false,
-  isTextColorChanged: false,
-  isBackColorChanged: false,
-  isLinkColorChanged: false,
-  isPhotoSens: false,
-  isReadingMask: false,
-  isReadingGuide: false,
-  isSpeech: false,
-  isDyslexicFont: false,
-  isBaskervilleFont: false,
-  isTranslated: false,
-}
 
-// function to reset all cookies
-const removeAllCookies = () => {
-  for (let i = 0; i < cookiesArr.length; i++) {
-    $.removeCookie(cookiesArr[i]);
-  }
-}
-
-// reset widgetItemObj on cookie load
-const isCookieActive = (input, value) => {
-  if (input && input !== value) {
-    return true
-  } else {
-    return false
-  }
-}
-
-const areCookiesSet = () => {
-  let updateCookies = widgetItemObj
-  updateCookies.isHighlighted = isCookieActive($.cookie("HighlightHover"), 'false')
-  updateCookies.isOutlined = isCookieActive($.cookie("HighlightLinks"), 'false')
-  updateCookies.isTextMag = isCookieActive($.cookie("TextMagnifier"), 'false')
-  updateCookies.isImgMag = isCookieActive($.cookie("ImageDescription"), 'false')
-  updateCookies.isFontBig = isCookieActive($.cookie("FontSizeCookie"), 'null')
-  updateCookies.isCursorBig = isCookieActive($.cookie("CursorEnlargeCookie"), 'null')
-  updateCookies.isLineHeightChanged = isCookieActive($.cookie("LinpageHeightVal"), 'inherit')
-  updateCookies.isWordSpaceChanged = isCookieActive($.cookie("WordSpaceVal"), 'inherit')
-  updateCookies.isLetterSpaceChanged = isCookieActive($.cookie("LetterSpaceVal"), 'inherit')
-  updateCookies.isLowSat = isCookieActive($.cookie("LowSaturationBackgroundCookie"), 'null')
-  updateCookies.isHighSat = isCookieActive($.cookie("HighSaturationBackgroundCookie"), 'null')
-  updateCookies.isInverted = isCookieActive($.cookie("InvertBackground"), 'null')
-  updateCookies.isDesaturated = isCookieActive($.cookie("DesaturatedBackground"), 'null')
-  updateCookies.isDarkContrast = isCookieActive($.cookie("DarkContrastBackground"), 'null')
-  updateCookies.isTextColorChanged = isCookieActive($.cookie("TextColorCookie"), 'false')
-  updateCookies.isBackColorChanged = isCookieActive($.cookie("BackgroundColorCookie"), 'false')
-  updateCookies.isLinkColorChanged = isCookieActive($.cookie("LinkColorCookie"), 'false')
-  updateCookies.isPhotoSens = isCookieActive($.cookie("PhotoSens"), 'false')
-  updateCookies.isReadingMask = isCookieActive($.cookie("ReadingMask"), 'false')
-  updateCookies.isReadingGuide = isCookieActive($.cookie("CursorGuide"), 'false')
-  updateCookies.isSpeech = isCookieActive($.cookie("TTS_click_enabled"), 'false')
-  updateCookies.isBaskervilleFont = isCookieActive($.cookie("BaskervilleFontCookie"), 'null')
-  updateCookies.isDyslexicFont = isCookieActive($.cookie("DyslexicFontCookie"), 'null')
-  return updateCookies
-}
-widgetItemObj = areCookiesSet()
-
-// check if widget items eval to true if so  fade in helper box
-const checkIfWidgetActive = () => {
-  if (Object.values(widgetItemObj).indexOf(true) > -1) {
-    isWidgetActive = true
-    $('#flourish_check_icon').fadeIn()
-    $('#toggle-flourish-list, #reset-flourish').fadeIn()
-  } else {
-    isWidgetActive = false
-    $('#flourish_check_icon').fadeOut()
-    $('#toggle-flourish-list, #reset-flourish, #item-delete-container').fadeOut()
-    $("#toggle-flourish-list").removeClass("fa-toggle-on");
-    $("#toggle-flourish-list").addClass("fa-toggle-off");
-    $.removeCookie('deleteContainerActive');
-  }
-}
-checkIfWidgetActive()
 
 
 //insert html for flourish to dom
@@ -148,7 +55,7 @@ const createFlourish = () => {
     </div>
 </section>
 <!-- The Modal -->
-<section id="flourish_widget" class="flourish_modal_background" aria-label="main flourish widget" aria-hidden="true">
+<section id="flourish_widget" class="flourish_modal_background" aria-label="main flourish widget" >
     <!-- Modal content -->
     <main id="flourish-modal-content" class="modal_content">
         <div class="flourish-modal-container d-flex justify-content-center flex-column align-items-center" role="container for flourish modal content">
@@ -162,7 +69,7 @@ const createFlourish = () => {
                     </div>
                     <div id="default-btn-container"
                         class="col-12 col-md-6 d-flex flex-wrap justify-content-center justify-content-md-end align-items-md-center">
-                        <button onclick="resetflourishModal()" type="button"
+                        <button  type="button"
                             class="btn mr-md-4 d-flex justify-content-center align-items-center" title="Reset Settings"
                             aria-label="Reset Settings" id="flourish_reset">
                             <img class="img-fluid flourish-reset-icon" aria-labelledby="flourish_reset"  loading="lazy" src="./flourish/img/reset.png"
@@ -1742,7 +1649,105 @@ const createFlourish = () => {
 
   `
 }
-createFlourish()
+
+// createFlourish()
+
+
+// code related to checking cookies/data/if widget is active or not
+// data to determine if widget is still active / widget active items
+let isWidgetActive = false
+const cookiesArr = ['TTS_click_enabled', 'DarkContrastBackground', 'DesaturateBackground', 'InvertBackground', 'HighSaturationBackground',
+  'LowSaturationBackground', 'AEC', '1P_JAR', 'NID', 'DV', 'translateLanguage', 'text-magnify-color-swatch', 'text-magnify-size-input', 'img-magnify-size-input', 'img-magnify-color-swatch', 'text-magnify-color-swatch', '.img-magnify-color-swatch', 'edit-reading-guide', 'edit-reading-mask', 'TTS_click_enabled', 'googtrans', 'readingMaskVal', 'BackgroundColorCookie', 'TextColorCookie', 'LinkColorCookie', 'TextMagnifier', 'HighlightLinks', 'ImageDescription', 'HighlightHover', 'FontSizeCookie', 'FT_Baskerville', 'FT_Dyslexic', 'CursorEnlargeCookie', 'PhotoSens', 'ReadingMask', 'CursorGuide', 'TTS_click_enabled', 'LinpageHeightVal', 'WordSpaceVal', 'LetterSpaceVal', 'speechPitch', 'speechRate', 'speechVol', 'voiceCookie']
+let widgetItemObj = {
+  isHighlighted: false,
+  isOutlined: false,
+  isTextMag: false,
+  isImgMag: false,
+  isFontBig: false,
+  isFontChanged: false,
+  isCursorBig: false,
+  isLineHeightChanged: false,
+  isWordSpaceChanged: false,
+  isLetterSpaceChanged: false,
+  'isDarkContrast': false,
+  'isDesaturated': false,
+  'isInverted': false,
+  'isHighSat': false,
+  'isLowSat': false,
+  isTextColorChanged: false,
+  isBackColorChanged: false,
+  isLinkColorChanged: false,
+  isPhotoSens: false,
+  isReadingMask: false,
+  isReadingGuide: false,
+  isSpeech: false,
+  isDyslexicFont: false,
+  isBaskervilleFont: false,
+  isTranslated: false,
+}
+
+// function to reset all cookies
+const removeAllCookies = () => {
+  for (let i = 0; i < cookiesArr.length; i++) {
+    $.removeCookie(cookiesArr[i]);
+  }
+}
+
+// reset widgetItemObj on cookie load
+const isCookieActive = (input, value) => {
+  if (input && input !== value) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const areCookiesSet = () => {
+  let updateCookies = widgetItemObj
+  updateCookies.isHighlighted = isCookieActive($.cookie("HighlightHover"), 'false')
+  updateCookies.isOutlined = isCookieActive($.cookie("HighlightLinks"), 'false')
+  updateCookies.isTextMag = isCookieActive($.cookie("TextMagnifier"), 'false')
+  updateCookies.isImgMag = isCookieActive($.cookie("ImageDescription"), 'false')
+  updateCookies.isFontBig = isCookieActive($.cookie("FontSizeCookie"), 'null')
+  updateCookies.isCursorBig = isCookieActive($.cookie("CursorEnlargeCookie"), 'null')
+  updateCookies.isLineHeightChanged = isCookieActive($.cookie("LinpageHeightVal"), 'inherit')
+  updateCookies.isWordSpaceChanged = isCookieActive($.cookie("WordSpaceVal"), 'inherit')
+  updateCookies.isLetterSpaceChanged = isCookieActive($.cookie("LetterSpaceVal"), 'inherit')
+  updateCookies.isLowSat = isCookieActive($.cookie("LowSaturationBackgroundCookie"), 'null')
+  updateCookies.isHighSat = isCookieActive($.cookie("HighSaturationBackgroundCookie"), 'null')
+  updateCookies.isInverted = isCookieActive($.cookie("InvertBackground"), 'null')
+  updateCookies.isDesaturated = isCookieActive($.cookie("DesaturatedBackground"), 'null')
+  updateCookies.isDarkContrast = isCookieActive($.cookie("DarkContrastBackground"), 'null')
+  updateCookies.isTextColorChanged = isCookieActive($.cookie("TextColorCookie"), 'false')
+  updateCookies.isBackColorChanged = isCookieActive($.cookie("BackgroundColorCookie"), 'false')
+  updateCookies.isLinkColorChanged = isCookieActive($.cookie("LinkColorCookie"), 'false')
+  updateCookies.isPhotoSens = isCookieActive($.cookie("PhotoSens"), 'false')
+  updateCookies.isReadingMask = isCookieActive($.cookie("ReadingMask"), 'false')
+  updateCookies.isReadingGuide = isCookieActive($.cookie("CursorGuide"), 'false')
+  updateCookies.isSpeech = isCookieActive($.cookie("TTS_click_enabled"), 'false')
+  updateCookies.isBaskervilleFont = isCookieActive($.cookie("BaskervilleFontCookie"), 'null')
+  updateCookies.isDyslexicFont = isCookieActive($.cookie("DyslexicFontCookie"), 'null')
+  return updateCookies
+}
+widgetItemObj = areCookiesSet()
+
+// check if widget items eval to true if so  fade in helper box
+const checkIfWidgetActive = () => {
+  if (Object.values(widgetItemObj).indexOf(true) > -1) {
+    isWidgetActive = true
+    $('#flourish_check_icon').fadeIn()
+    $('#toggle-flourish-list, #reset-flourish').fadeIn()
+  } else {
+    isWidgetActive = false
+    $('#flourish_check_icon').fadeOut()
+    $('#toggle-flourish-list, #reset-flourish, #item-delete-container').fadeOut()
+    $("#toggle-flourish-list").removeClass("fa-toggle-on");
+    $("#toggle-flourish-list").addClass("fa-toggle-off");
+    $.removeCookie('deleteContainerActive');
+  }
+}
+checkIfWidgetActive()
+
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
@@ -4127,12 +4132,12 @@ const genLanguageBtns = (arr, destination) => {
     langBtnContainer.classList.add('flourish-language-btn-container', "col-6", "col-sm-4", "col-lg-3")
     let divId = arr[i].LanguageCodeGoogleTrans ? arr[i].LanguageCodeGoogleTrans : `${arr[i].LanguageEnglish}-language-btn`
     langBtnContainer.innerHTML = `
-      <div id="${divId}" class="flourish-language-btn lang-translate-selector">
-          <img loading="lazy" class="language-icons" src="./flourish/img/language/${arr[i].FlorishIconfilename}"
-              alt="${arr[i].LanguageEnglish}-${arr[i].LanguageAutonym} Language icon">
-              <span class="translate-language-span notranslate">${arr[i].LanguageAutonym}</span>
-      </div>
-      `
+        <button id="${divId}" class="flourish-language-btn lang-translate-selector">
+            <img loading="lazy" class="language-icons" src="./flourish/img/language/${arr[i].FlorishIconfilename}"
+                alt="${arr[i].LanguageEnglish}-${arr[i].LanguageAutonym} Language icon">
+                <span class="translate-language-span notranslate">${arr[i].LanguageAutonym}</span>
+        </button>
+        `
     destination.append(langBtnContainer)
   }
   const flourishLangSearchBtn = document.getElementById('flourish-language-search')
@@ -4201,15 +4206,15 @@ const genSearchBtns = (arr) => {
     searchListItem.classList.add('search-list-item-container', 'notranslate')
     let divId = arr[i].LanguageCodeGoogleTrans ? arr[i].LanguageCodeGoogleTrans : `${arr[i].LanguageEnglish}-language-btn`
     searchListItem.innerHTML = `
-      <div id="${divId}" class="search-list-item lang-translate-selector">
-          <div class="search-item-thumbnail">
-              <img src="./flourish/img/language/${arr[i].FlorishIconfilename}" alt="${arr[i].LanguageEnglish}-${arr[i].LanguageAutonym} Language icon">
-          </div>
-          <div class="search-item-info">
-              <p>${arr[i].LanguageAutonym}</p>
-          </div>
-      </div>
-    `
+        <button id="${divId}" class="search-list-item lang-translate-selector">
+            <div class="search-item-thumbnail">
+                <img src="./flourish/img/language/${arr[i].FlorishIconfilename}" alt="${arr[i].LanguageEnglish}-${arr[i].LanguageAutonym} Language icon">
+            </div>
+            <div class="search-item-info">
+                <p>${arr[i].LanguageAutonym}</p>
+            </div>
+        </button>
+      `
     searchList.append(searchListItem)
   }
   langBtnClickHandler()
@@ -4393,7 +4398,7 @@ const genAccordionItems = (currItem, destination, needsIcons) => {
   accordionRow.classList.add('flourish-accordion-item')
   accordionRow.id = `${formatCurrItem}-accordion`
   accordionRow.setAttribute("data-filter-id", currItem);
-  const accordionBtn = document.createElement('div')
+  const accordionBtn = document.createElement('button')
   accordionBtn.classList.add('flourish-accordion-header')
   const header = document.createElement('h5')
   header.innerText = `${currItem}`
@@ -4402,8 +4407,8 @@ const genAccordionItems = (currItem, destination, needsIcons) => {
     const imgContainer = document.createElement('div')
     imgContainer.classList.add('d-flex', 'justify-content-center', 'align-items-center', 'ml-2')
     imgContainer.innerHTML = `
-      <img class="geo-location-icons" src="./flourish/img/${formatCurrItem}.png" alt="${formatCurrItem} icon">
-      `
+        <img class="geo-location-icons" src="./flourish/img/${formatCurrItem}.png" alt="${formatCurrItem} icon">
+        `
     imgContainer.append(header)
     accordionBtn.append(imgContainer)
   } else {
@@ -4513,7 +4518,7 @@ const setFlourishIconPlacement = () => {
     flourishMain.classList.add('trigger-large')
   }
 }
-setFlourishIconPlacement()
+// setFlourishIconPlacement()
 
 // const changePopoverPlacement = () => {
 //   const flourishMain = document.querySelector('#flourish-widget-main')
@@ -4525,15 +4530,11 @@ setFlourishIconPlacement()
 // }
 // changePopoverPlacement()
 
-window.addEventListener("resize", () => {
-  setFlourishIconPlacement()
-  // changePopoverPlacement()
-});
 const initAudioIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-volume-up trigger-audio-icon" viewBox="0 0 16 16">\
-<path d="M11.536 14.01A8.47 8.47 0 0 0 14.026 8a8.47 8.47 0 0 0-2.49-6.01l-.708.707A7.48 7.48 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303z"/>\
-<path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.48 5.48 0 0 1 11.025 8a5.48 5.48 0 0 1-1.61 3.89z"/>\
-<path d="M10.025 8a4.5 4.5 0 0 1-1.318 3.182L8 10.475A3.5 3.5 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.5 4.5 0 0 1 10.025 8M7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11"/>\
-</svg>'
+  <path d="M11.536 14.01A8.47 8.47 0 0 0 14.026 8a8.47 8.47 0 0 0-2.49-6.01l-.708.707A7.48 7.48 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303z"/>\
+  <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.48 5.48 0 0 1 11.025 8a5.48 5.48 0 0 1-1.61 3.89z"/>\
+  <path d="M10.025 8a4.5 4.5 0 0 1-1.318 3.182L8 10.475A3.5 3.5 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.5 4.5 0 0 1 10.025 8M7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11"/>\
+  </svg>'
 
 const resetAudioIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/><path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/></svg>'
 
@@ -4819,6 +4820,7 @@ $(function () {
       toggleSpeechOn()
     } else {
       toggleSpeechOff()
+      resetSpeech()
     }
   });
 });
@@ -4984,10 +4986,10 @@ const addAudioPlayers = (selectors) => {
 const generateAudioPlayer = (item) => {
   $(item).wrap('<section class="TTS_content"></section>');
   $(`<div class="audio_state">\
-    <button class="trigger-audio play-pause inactive-item audio-inactive" title="Trigger audio">${initAudioIcon}</button>\
-    <button class="trigger-audio reset-audio-btn d-none" title="Cancel">\
-    ${resetAudioIcon}
-    </button></div>`).insertAfter(item);
+      <button class="trigger-audio play-pause inactive-item audio-inactive" title="Trigger audio">${initAudioIcon}</button>\
+      <button class="trigger-audio reset-audio-btn d-none" title="Cancel">\
+      ${resetAudioIcon}
+      </button></div>`).insertAfter(item);
 }
 
 /***** handler for speech items -- the speech btn and the play/pause/reset all have trigger audio class.. tehse conditionals test whcih one is which *****/
@@ -5108,8 +5110,8 @@ const addWidgetControls = (item, text) => {
     const listItem = document.createElement('li')
     listItem.classList.add(item, 'fade-in', 'close-list-items')
     listItem.innerHTML = `<svg class="close-active-item" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
-    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"></path>
-  </svg> <span class="close-active-text">${text}</span> `
+      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"></path>
+    </svg> <span class="close-active-text">${text}</span> `
     widgetList.append(listItem)
     languageModalSpeechHandler('.close-active-text')
   }
@@ -6967,8 +6969,15 @@ const resetflourishModal = () => {
   //  $(".audio_state").hide()
 }
 
+document.querySelector('#flourish_reset').addEventListener('click', () => {
+  resetflourishModal()
+})
+
 let resetIcon = document.getElementById('reset-flourish')
 resetIcon.addEventListener('click', () => {
   resetflourishModal()
 })
+
+
+
 
